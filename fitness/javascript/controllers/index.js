97c6f92664d5b61c34ff8ce095b6b5480186ec64
Bruce Martin <bmartin@heiskell.com>
2022-03-30 09:27:24 -0700

    updates

--- fitness/javascript/controllers/index.js
+++ fitness/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 import { Application } from "@hotwired/stimulus";
 import DatePickerController from "./date_picker_controller";
+import ChartsController from "./charts_controller";
 
-stimulus = Application.start()
-stimulus.register("datepicker", DatePickerController)
+stimulus = Application.start();
+stimulus.register("datepicker", DatePickerController);
+stimulus.register("charts", ChartsController);
